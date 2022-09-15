387f15019d89825d4e3368f70e96496ecf8493ec
tenderking <gmu003@gmail.com>
2022-09-15 22:46:01 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import TheCards from "./components/TheCards.jsx";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const cardInfo = {
+		cardHolderName: "Jane Appleseed",
+		cardNumber: "0000 0000 0000 0000",
+		expiryDate: "00/00",
+		cvc: "000",
+	};
+	return (
+		<div className="App">
+			<header className="App-header">
+				<TheCards {...cardInfo} />
+			</header>
+			<main>
+				<form>
+					<div className="w-100">
+						<p className="Form--label">Card Holder Name</p>
+						<input type="text" placeholder="e.g. Jane Appleseed" />
+					</div>
+					<div className="w-100">
+						<p className="Form--label">Card Number</p>
+						<input type="text" placeholder="e.g 1234 5678 9123 000" />
+					</div>
+
+					<div className="flex">
+						<div className="w-100">
+							<p className="Form--label">EXP.DATE(MM/YY)</p>
+							<div className="flex w-100">
+								<input type="text" placeholder="MM" />
+								<input type="text" placeholder="YY" />
+							</div>
+						</div>
+						<div className="w-100">
+							<p className="Form--label">CVC</p>
+							<input type="text" placeholder="e.g 123" />
+						</div>
+					</div>
+					<button className="btn">Confirm</button>
+				</form>
+			</main>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/TheCards.jsx
@@ -0,0 +1,23 @@
+import bgCardback from "../assets/images/bg-card-back.png";
+import bgCardFront from "../assets/images/bg-card-front.png";
+import cardLogo from "../assets/images/card-logo.svg"
+function TheCards(props) {
+  return <>
+    <div className="App-header__container" >
+      <div className="c-card--back">
+
+      <img className="c-card--back__container" src={bgCardback} alt="header img" />
+      <p className="c-card--cvc">{props.cvc }</p>
+      </div>
+      <div className="c-card--front" >
+        <img className="c-card--front__container" src={bgCardFront} alt="header img" />
+      <img className="c-card--logo" src={cardLogo} alt="header img" />
+        <p className="c-card--number">{props.cardNumber}</p>
+        <p className="c-card--name"> {props.cardHolderName}</p>
+        <p className="c-card--expiry">{ props.expiryDate}</p>
+      </div>
+    </div>
+  </>
+}
+
+export default TheCards;
